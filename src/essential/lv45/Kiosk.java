fb8e49fd1841bef7c67f1a3df5dd3697849e34fb
Gaeso <ghwns2239@gmail.com>
2025-04-28 13:42:45 +0900

    feat : Kiosk 기능을 OrderSystem으로 책임 분리

--- src/essential/lv45/Kiosk.java
+++ src/essential/lv45/Kiosk.java
@@ -7,6 +7,7 @@ public class Kiosk {
 
     private final List<Menu> menuList;
     private final Scanner sc = new Scanner(System.in);
+    private final OrderSystem os = new OrderSystem();
 
     public Kiosk(List<Menu> menuList) {
         this.menuList = menuList;
@@ -21,52 +22,27 @@ public class Kiosk {
             int selectedItemNumber = -1;
             int i = 1;
 
-            System.out.println("[ MAIN MENU ]");
-            for(Menu menu : menuList)
-            {
-                System.out.println(i + ". " + menu.getMenuName());
-                i++;
-            }
-            System.out.println("0. 종료");
+            // 메뉴 목록 출력
+            os.showAllMenu(i, menuList);
 
             // 메뉴 선택 입력문
-            try {
-                selectedMenuNumber = Integer.parseInt(sc.nextLine());
-                if(selectedMenuNumber < 0 || selectedMenuNumber > menuList.size()) {
-                    System.out.println("올바른 카테고리를 선택해주세요.");
-                    continue;
-                }
-                if(selectedMenuNumber == 0)
-                    continue;
-            } catch (NumberFormatException e) {
-                System.out.println("숫자만 입력해주세요.");
+            selectedMenuNumber = os.validateAndSelectMenu(menuList);
+            if(os.shouldContinue())
                 continue;
-            }
 
             // 아이템 선택 반복문
             while(selectedItemNumber != 0) {
-                menuList.get(selectedMenuNumber-1).showMenuItems();
-                System.out.println("0. 뒤로가기");
 
-                // 아이템 선택 입력문
-                try {
-                    selectedItemNumber = Integer.parseInt(sc.nextLine());
-                    if(selectedItemNumber < 0 || selectedItemNumber > menuList.get(selectedMenuNumber-1).getMenuItemList().size()) {
-                        System.out.println("올바른 메뉴를 선택해주세요.");
-                        continue;
-                    }
-                    if(selectedItemNumber == 0)
-                    {
-                        continue;
-                    }
-                } catch (NumberFormatException e) {
-                    System.out.println("숫자만 입력해주세요.");
-                }
+                // 아이템 목록 출력
+                os.showAllItems(menuList, selectedMenuNumber);
 
-                List<MenuItem> menuItems = menuList.get(selectedMenuNumber-1).getMenuItemList();
-                MenuItem menuItem = menuItems.get(selectedItemNumber-1);
-                System.out.println("선택된 메뉴 : " + menuItem.getItemName() + " | W " + menuItem.getPrice() + " | " +
-                        menuItem.getFoodDescription());
+                // 아이템 선택 입력
+                selectedItemNumber = os.validateAndSelectItems(menuList, selectedMenuNumber);
+                if(os.shouldContinue())
+                    continue;
+
+                // 선택 아이템 출력
+                os.showSelectedItem(menuList, selectedMenuNumber, selectedItemNumber);
             }
         }
         System.out.println("주문을 종료합니다.");
--- src/essential/lv45/OrderSystem.java
+++ src/essential/lv45/OrderSystem.java
@@ -1,6 +1,80 @@
 package essential.lv45;
 
+import java.util.List;
+import java.util.Scanner;
+
 // 메뉴 출력, 사용자의 입력 처리를 관리하는 클래스
 public class OrderSystem {
 
+    private final Scanner sc = new Scanner(System.in);
+    private boolean breakFlag = false;
+
+    public void showAllMenu(int i, List<Menu> menuList) {
+        System.out.println("[ MAIN MENU ]");
+        for(Menu menu : menuList)
+        {
+            System.out.println(i + ". " + menu.getMenuName());
+            i++;
+        }
+        System.out.println("0. 종료");
+    }
+
+    public int validateAndSelectMenu(List<Menu> menuList) {
+        breakFlag = false;
+        try {
+            int selectedMenuNumber = Integer.parseInt(sc.nextLine());
+            if(selectedMenuNumber < 0 || selectedMenuNumber > menuList.size()) {
+                System.out.println("올바른 카테고리를 선택해주세요.");
+                breakFlag = true;
+                return -1;
+            }
+            if(selectedMenuNumber == 0) {
+                breakFlag = true;
+                return -1;
+            }
+            return selectedMenuNumber;
+        } catch (NumberFormatException e) {
+            System.out.println("숫자만 입력해주세요.");
+            breakFlag = true;
+            return -1;
+        }
+    }
+
+    public void showAllItems(List<Menu> menuList, int selectedMenuNumber) {
+        menuList.get(selectedMenuNumber-1).showMenuItems();
+        System.out.println("0. 뒤로가기");
+    }
+
+    public int validateAndSelectItems(List<Menu> menuList,int selectedMenuNumber) {
+        breakFlag = false;
+        try {
+            int selectedItemNumber = Integer.parseInt(sc.nextLine());
+            if(selectedItemNumber < 0 || selectedItemNumber > menuList.get(selectedMenuNumber-1).getMenuItemList().size()) {
+                System.out.println("올바른 메뉴를 선택해주세요.");
+                breakFlag = true;
+                return -1;
+            }
+            if(selectedItemNumber == 0)
+            {
+                breakFlag = true;
+                return -1;
+            }
+            return selectedItemNumber;
+        } catch (NumberFormatException e) {
+            System.out.println("숫자만 입력해주세요.");
+            breakFlag = true;
+            return -1;
+        }
+    }
+
+    public void showSelectedItem(List<Menu> menuList, int selectedMenuNumber, int selectedItemNumber) {
+        List<MenuItem> menuItems = menuList.get(selectedMenuNumber-1).getMenuItemList();
+        MenuItem menuItem = menuItems.get(selectedItemNumber-1);
+        System.out.println("선택된 메뉴 : " + menuItem.getItemName() + " | W " + menuItem.getPrice() + " | " +
+                menuItem.getFoodDescription());
+    }
+
+    public boolean shouldContinue() {
+        return breakFlag;
+    }
 }
